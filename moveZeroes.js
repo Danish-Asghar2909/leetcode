36a38ea9b5f141fe8d7da642a3f9fc0a9edc03b1
Danish-Asghar2909 <61777716+Danish-Asghar2909@users.noreply.github.com>
2023-01-13 19:55:06 +0530

    moveZeroes [not-accpeted}

---
+++ moveZeroes.js
@@ -0,0 +1,22 @@
+// Given an integer array nums, move all 0's to the end of it while maintaining the relative order of the non-zero elements.
+
+// Note that you must do this in-place without making a copy of the array.
+
+
+var moveZeroes = function(nums) {
+    let arrOnlyContainZero = []
+    let arrWithoutZero = []
+    for(let i = 0 ; i < nums.length ;i++){
+        if(nums[i]===0){
+            arrOnlyContainZero.push(nums[i])
+        }else{
+            arrWithoutZero.push(nums[i])
+        }
+    }
+    //console.log("sample : ", nums , arrOnlyContainZero , arrWithoutZero )
+    return [...arrWithoutZero , ...arrOnlyContainZero]
+};
+
+const nums = [0,1,0,3,12]
+const output = moveZeroes(nums)
+console.log("Output : ", output)
