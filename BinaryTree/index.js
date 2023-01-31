13b7f15128b9908d2aa762ebcd988b300d00a65f
Danish-Asghar2909 <danish.2909@icloud.com>
2023-01-31 08:23:28 +0530

    Binary Tree practise version 2

--- BinaryTree/index.js
+++ BinaryTree/index.js
@@ -105,3 +105,93 @@ const hasIncludesRecursive = (root , target) => {
 const output5 = hasIncludesRecursive(a, 'f')
 console.log("Output has includes : ", output5)
 //---------------------------------------------------------------------------------------------------
+
+//Tree Sum
+const three = new Node(3)
+const eleven = new Node(11)
+const four = new Node(4)
+const five = new Node(5)
+const two = new Node(2)
+const one = new Node(1)
+
+
+three.left = eleven
+three.right = four
+eleven.left = five
+eleven.right = two
+four.right = one
+
+  //         3
+  //       /   \
+  //     11     4
+  //    /  \     \
+  //   5    2     1
+
+  //depth first iterative
+  const treeSumIterative = (root) =>{
+   
+    if(!root) return 0
+    let sum = 0
+    let stack = [root]
+    while(stack.length > 0){
+      const current = stack.pop()
+   
+      sum += current.val
+      if(current.right) stack.push(current.right)
+      if(current.left) stack.push(current.left)
+    }
+    return sum
+  }
+
+  const output6 = treeSumIterative(three)
+  console.log('Tree Sum iterative : ', output6)
+
+
+  // Tree Sum Recursive
+  const treeSumRecursive = ( root ) =>{
+    if(!root) return 0;
+    return root.val + treeSumRecursive(root.left) + treeSumRecursive(root.right)
+  }
+  const output7 = treeSumRecursive(three)
+  console.log("tree sum recursive : ", output7)
+  //-----------------------------------------------------------------------------------
+
+  //Min Value in Tree
+
+  const minValueRecursive = (root) => {
+    if(!root) return Infinity
+    return Math.min(root.val , minValueRecursive(root.left) , minValueRecursive(root.right))
+  } 
+
+  const output8 = minValueRecursive(three)
+  console.log("Min Value recursively : ", output8)
+
+  //Min Value Iterative (Breadth Search Approach)
+  const minSearchIterative = (root) =>{
+    if(!root) return 0
+    let minValue = Infinity
+    const queue = [root]
+    while(queue.length>0){
+      const current = queue.shift()
+      if(current.val < minValue) minValue = current.val
+      if(current.left) queue.push(current.left)
+      if(current.right) queue.push(current.right)
+    }
+    return minValue
+  }
+
+  const output9 = minSearchIterative(three)
+
+  console.log("min value iterative : ", output9)
+
+  //Max Root Path Sum
+
+  const maxRootPathSum = ( root ) =>{
+    if(!root) return -Infinity
+    if(!root.left && !root.right) return root.val // for leaf node
+    const maxValue = Math.max( maxRootPathSum(root.left) , maxRootPathSum(root.right))
+    return root.val + maxValue
+  }
+
+  const output10 = maxRootPathSum(three)
+  console.log("max root path : ", output10)
